51490404ef4c5ef342f95e0fb69dc0043bd7c158
cistus75 <100517126+cistus75@users.noreply.github.com>
2026-08-27 17:56:03 +0900

    style: load SVG battlefield overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './alignment.css'
+import './battlefield-svg.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
