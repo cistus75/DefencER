5f02cc0acc9aa637168904b9acb6aeb126e9e54d
cistus75 <100517126+cistus75@users.noreply.github.com>
2026-08-27 17:45:53 +0900

    style: enable quarter-view battlefield layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './alignment.css'
+import './battlefield-3d.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
