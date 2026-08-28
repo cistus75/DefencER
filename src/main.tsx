548ba8f1888aa6269ba0952f0448e33925147bbe
cistus75 <100517126+cistus75@users.noreply.github.com>
2026-08-28 09:07:44 +0900

    style: apply laboratory tone overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './alignment.css'
 import './battlefield-svg.css'
+import './lab-tone.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
